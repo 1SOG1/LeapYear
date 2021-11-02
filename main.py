c6652ff23be267f8fd93b6b8c67013f3fad7d82f
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2021-11-02 20:56:27 +0100

    housekeeping, Added leap_year_check_function.py to git

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pytest
 from leap_year_check_function import leap_year_check
 
 print("==== Leap Year Checker 3000 ====\n\n")
@@ -12,8 +11,3 @@ if leap_year_check(year_to_check):
     print(f"\n{year_to_check} is a leap year! boy-oh boy... what will you do with all this time?!\n")
 else:
     print(f"\n{year_to_check} is not a leap year :-( dont get to leap into the couch one extra day this year i guess\n")
-
-
-
-
-
